b54d170b89e6653a7325443c2aa00a0cf47d8b1d
ktochow1 <ktochow1@binghamton.edu>
2018-10-14 19:11:13 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_all?(collection)
   i = 0
   while i < collection.length 
-    yield(collection[i]) binding.pry
-    i = i + 1 
+    yield(collection[i]) 
+    i = i + 1 binding.pry
   end
 end
